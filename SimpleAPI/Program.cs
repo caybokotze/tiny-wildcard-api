4e59e4486ec6f5ecb2da358ffe08cd25dd304d7b
Caybo Kotzé <caybokotze@gmail.com>
2023-08-10 18:07:19 +0200

    add required files

---
+++ SimpleAPI/Program.cs
@@ -0,0 +1,20 @@
+using System.Text;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Map("/{**any}", async context =>
+{
+    Console.WriteLine(context.Request.Path);
+
+    var memoryStream = new MemoryStream();
+    await context.Request.Body.CopyToAsync(memoryStream);
+    var bytes = memoryStream.ToArray();
+    
+    Console.WriteLine(Encoding.UTF8.GetString(bytes));
+    
+    Console.WriteLine(JsonSerializer.Serialize(context.Request.Form, new JsonSerializerOptions{WriteIndented = true}));
+});
+
+app.Run();
